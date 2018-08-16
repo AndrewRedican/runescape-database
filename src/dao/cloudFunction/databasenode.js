a62e6f7dbf141cc78aa6cb4a6aa891ecb84c34e7
Andrew Redican <andrew.redican.mejia@gmail.com>
2018-08-16 09:02:08 -0600

    database dao for cloud function

--- src/dao/cloudFunction/databasenode.js
+++ src/dao/cloudFunction/databasenode.js
@@ -1,34 +1,34 @@
-function cloudFunctionNode ( ) {
-    
+import { isNotType } from '../../morphs/err'
+
+function cloudFunctionNode ( pathName, name, description, enabled=true ) {
+    isNotType('pathName',pathName,'string')
+    isNotType('name',name,'string')
+    isNotType('description',description,'string')
+    isNotType('enabled',enabled,'boolean')
+    this.path = `system/process/cloudFunctions/tasks/${pathName}`
+    this.default = {
+        name         : name,
+        description  : description,
+        enabled      : enabled,
+        lastExecuted : false,
+        lastModified : false,
+        logs: {
+            count : false,
+            data  : false,
+            first : false,
+            last  : false,
+            timeframe: {
+                milliseconds: false,
+                text: false
+            }
+        },
+        control : false,
+        status : {
+            description : false,
+            progress    : false,
+            running     : false
+        }
+    }
 }
 
-
-control: 
-false
-description: 
-"Fetches latest updates from RuneScape database ..."
-enabled: 
-true
-lastExecuted: 
-false
-lastModified: 
-false
-logs
-count: 
-false
-data: 
-false
-first: 
-false
-last: 
-false
-timeframe
-name: 
-"Sync App to RuneScape database"
-status
-description: 
-false
-progress: 
-false
-running: 
-false
+module = module.exports = cloudFunctionNode
