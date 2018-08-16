ddf662defc34317f7c7d2917967bc64fe0f80bed
Andrew Redican <andrew.redican.mejia@gmail.com>
2018-08-16 08:58:02 -0600

    err upgrade

--- src/morphs/err.js
+++ src/morphs/err.js
@@ -1,82 +1,74 @@
-import {
-    getType,
-    locate
-} from 'mitsuketa'
+import { getType, locate } from 'mitsuketa'
 
-const err = {
-    getCaller: (skip = 1) => {
+export function getCaller (skip = 1) {
         const stackTrace = (new Error()).stack
         var callerName = stackTrace.replace(/^Error\s+/, '')
-        callerName = callerName.split('\n')[skip]
+        callerName = callerName.split("\n")[skip]
         callerName = callerName.replace(/^\s+at Object./, '').replace(/^\s+at /, '').replace(/ \(.+\)$/, '')
         return callerName
-    },
-
-    throwError: (fxName = 'unknown function', paramName = 'unknown parameter', expectation = 'to be defined') => {
-        throw ['@', fxName, '(): Expected parameter \'', paramName, '\' ', expectation].join('')
-    },
-
-    isUndefined: (paramName = '<unknown parameter>', param) => {
-        if ([null, undefined].indexOf(param) > -1) err.throwError(err.getCaller(2), paramName)
-    },
-
-    isFalsy: (paramName = '<unknown parameter>', param) => {
-        if (!param) err.throwError(err.getCaller(2), paramName)
-    },
-
-    isNoneOf: (paramName = '<unknown parameter>', param, contains = []) => {
-        if (contains.indexOf(param) === -1) err.throwError(err.getCaller(2), paramName, 'to be any of' + JSON.stringify(contains))
-    },
-
-    isAnyOf: (paramName = '<unknown parameter>', param, contains = []) => {
-        if (contains.indexOf(param) > -1) err.throwError(err.getCaller(2), paramName, 'not to be any of' + JSON.stringify(contains))
-    },
-
-    isNotType: (paramName = '<unknown parameter>', param, type = '') => {
-        if (getType(param) !== type.toLowerCase()) err.throwError(err.getCaller(2), paramName, 'to be type ' + type.toLowerCase())
-    },
-
-    isAnyTypeOf: (paramName = '<unknown parameter>', param, types = []) => {
-        types.forEach(type => {
-            if (getType(param) === type) err.throwError(err.getCaller(2), paramName, 'not to be type of ' + type.toLowerCase())
-        })
-    },
-
-    missingKey: (paramName = '<unknown parameter>', param, keyName = '') => {
-        err.isUndefined(paramName, param)
-        if (Object.keys(param).indexOf(keyName) === -1) err.throwError(err.getCaller(2), paramName, 'to contain \'' + keyName + '\' key')
-    },
-
-    missingAnyKeys: (paramName = '<unknown parameter>', param, keyNames = ['']) => {
-        err.isUndefined(paramName, param)
-        const keyList = Object.keys(param)
-        keyNames.forEach(keyName => {
-            if (keyList.indexOf(keyName) === -1) err.throwError(err.getCaller(2), paramName, 'to contain \'' + keyName + '\' key')
-        })
-    },
-
-    containsUndefined: (paramName = '<unknown parameter>', param) => {
-        [undefined, null].forEach(value => {
-            const location = locate(param, value)
-            if (location) err.throwError(err.getCaller(2), paramName, 'not to contain \'' + JSON.stringify(value) + '\' at ' + location)
-        })
-    },
-
-    isInvalidPath: (paramName = '<unknown parameter>', param) => {
-        err.isUndefined(paramName, param)
-        err.isNotType(paramName, param, 'string')
-        err.isAnyOf(paramName, param, ['', '/'])
-        '.$[]#'.split().forEach(invalidChar => {
-            if (param.indexOf(invalidChar) > -1) err.throwError(err.getCaller(2), paramName, 'not to contain invalid character \'' + invalidChar + '\'')
-        })
-        if (param.match(/\/{2,}/g)) err.throwError(err.getCaller(2), paramName, 'not to contain consecutive forward slash characters')
-    },
-
-    isInvalidWriteData: (paramName = '<unknown parameter>', param) => {
-        err.isUndefined(paramName, param)
-        err.containsUndefined(paramName, param)
-    }
-
 }
 
-export default err
+export function throwError (fxName = 'unknown function', paramName = 'unknown parameter', expectation = 'to be defined') {
+    throw `@${fxName}(): Expected parameter '${paramName}' ${expectation}`
+}
+
+export function isUndefined (paramName = '<unknown parameter>', param) {
+    if ([null, undefined].indexOf(param) > -1) throwError(getCaller(2), paramName)
+}
+
+export function isFalsy (paramName = '<unknown parameter>', param) {
+    if (!param) throwError(getCaller(2), paramName)
+}
+
+export function isNoneOf (paramName = '<unknown parameter>', param, contains = []) {
+    if (contains.indexOf(param) === -1) throwError(getCaller(2), paramName, 'to be any of' + JSON.stringify(contains))
+}
+
+export function isAnyOf (paramName = '<unknown parameter>', param, contains = []) {
+    if (contains.indexOf(param) > -1) throwError(getCaller(2), paramName, 'not to be any of' + JSON.stringify(contains))
+}
+
+export function isNotType (paramName = '<unknown parameter>', param, type = '') {
+    if (getType(param) !== type.toLowerCase()) throwError(getCaller(2), paramName, 'to be type ' + type.toLowerCase())
+}
+
+export function isAnyTypeOf (paramName = '<unknown parameter>', param, types = []) {
+    types.forEach( type => {
+        if (getType(param) === type) throwError(getCaller(2), paramName, 'not to be type of ' + type.toLowerCase())
+    })
+}
+
+export function missingKey (paramName = '<unknown parameter>', param, keyName = '') {
+    isUndefined(paramName, param)
+    if (Object.keys(param).indexOf(keyName) === -1) throwError(getCaller(2), paramName, `to contain '${keyName}' key`)
+}
+
+export function missingAnyKeys (paramName = '<unknown parameter>', param, keyNames = ['']) {
+    isUndefined(paramName, param)
+    const keyList = Object.keys(param)
+    keyNames.forEach( keyName => {
+        if (keyList.indexOf(keyName) === -1) throwError(getCaller(2), paramName, `to contain '${keyName}' key`)
+    })
+}
+
+export function containsUndefined (paramName = '<unknown parameter>', param) {
+    [undefined, null].forEach( value => {
+        const location = locate(param, value)
+        if (location) throwError(getCaller(2), paramName, `not to contain '${JSON.stringify(value)}' at ${location}`)
+    })
+}
+
+export function isInvalidPath (paramName = '<unknown parameter>', param) {
+    isUndefined(paramName, param)
+    isNotType(paramName, param, 'string')
+    isAnyOf(paramName, param, ['', '/'])
+    '.$[]#'.split().forEach( invalidChar => {
+        if (param.indexOf(invalidChar) > -1) throwError(getCaller(2), paramName, `not to contain invalid character '${invalidChar}'`)
+    })
+    if (param.match(/\/{2,}/g)) throwError(getCaller(2), paramName, 'not to contain consecutive forward slash characters')
+}
+
+export function isInvalidWriteData (paramName = '<unknown parameter>', param) {
+    isUndefined(paramName, param)
+    containsUndefined(paramName, param)
+}
